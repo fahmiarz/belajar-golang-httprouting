453b1d04e29fc0af4b2c7049eed913d7c883090d
Fahmi Arzalega <fahmiarza@gmail.com>
2024-11-21 08:50:42 +0700

    Belajar Golang Http Routing

---
+++ not_found_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestNotFound(t *testing.T) {
+	router := httprouter.New()
+	router.NotFound = http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Kaga Ketemu nih")
+	})
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:3000/404", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	assert.Equal(t, "Kaga Ketemu nih", string(body))
+}
